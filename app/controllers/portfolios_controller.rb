2d1cadebb5d61bcc2c31b8c9b338c86b875ab3a3
MVBharat <manebharat85@gmail.com>
2018-10-30 16:44:44 +0530

    Add Delete operation to portfolio function

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -38,4 +38,14 @@ class PortfoliosController < ApplicationController
 	def show
 		@portfolio_item = Portfolio.find(params[:id])		
 	end
+
+	def destroy
+		@portfolio_item = Portfolio.find(params[:id])
+		@portfolio_item.destroy
+
+		respond_to do |format|
+			format.html { redirect_to portfolios_path, notice: 'Item is destroied' }
+		end
+
+	end
 end
