63a6572576875d34824267a906c1f3316dc426e1
MVBharat <manebharat85@gmail.com>
2018-10-31 18:46:51 +0530

    add custom image to concern model

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,12 @@
 class Skill < ApplicationRecord
+	include Placeholder
+
 	validates_presence_of :title, :percent_utilized
+
+	after_initialize :set_defaults
+
+	def set_defaults
+		self.main_image ||= Placeholder.image_generator(height: '250', width: '250')
+	end
+
 end
