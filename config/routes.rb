96030e5f8658f3ad935d83539e3a7e1bf61837c7
MVBharat <manebharat85@gmail.com>
2018-11-05 14:07:51 +0530

    customization register login logut routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios, except: [:show]
   get 'portfolios/:id', to: 'portfolios#show', as: 'portfolio_show'
 
