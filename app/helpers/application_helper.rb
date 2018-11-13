da1e712adbabc33510e517235bfe09248a1c561d
MVBharat <manebharat85@gmail.com>
2018-11-13 12:16:30 +0530

    Add helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,5 @@
 module ApplicationHelper
 
-<<<<<<< HEAD
-	def login_helper
-		if current_user.is_a?(User)
-			link_to "Log out", destroy_user_seession_path, method: :delete
-		else
-			(link_to "Register", new_registration_path) + 
-			"<br>".html_sage + 
-			(link_to "Login", new_user_session_path)
-		end		
-=======
 	def login_helper		
 		if current_user.is_a?(User) 
 			link_to "Logout", destroy_user_session_path, method: :delete
@@ -18,6 +8,5 @@ module ApplicationHelper
 			"<br>".html_safe +
 			(link_to "Login", new_user_session_path)
 		end 
->>>>>>> 222b4792acac27741df34589a99456fdab647491
 	end
 end
