23bcce7b58cef9802c3f857d48c744c05dae23a7
MVBharat <manebharat85@gmail.com>
2018-11-13 11:42:22 +0530

    adding login helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	def login_helper
+		if current_user.is_a?(User)
+			link_to "Log out", destroy_user_seession_path, method: :delete
+		else
+			(link_to "Register", new_registration_path) + 
+			"<br>".html_sage + 
+			(link_to "Login", new_user_session_path)
+		end		
+	end
 end
