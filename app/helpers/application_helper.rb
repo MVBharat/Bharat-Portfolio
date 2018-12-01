5e2315f0c91cbc7843c04dcf8e5d2040d62f3dc4
MVBharat <manebharat85@gmail.com>
2018-12-01 18:10:28 +0530

    Implemented refactor for navigation and active status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,5 +20,52 @@ module ApplicationHelper
 	def copyright_generator
   		@copyright = ManeViewTool::Renderer.copyright 'Bharat Mane', 'All rights reserved'		
 	end
+
+	def nav_items
+		[
+			{
+				url: root_path,
+				title: 'Home'
+			},			{
+				url: about_path,
+				title: 'About Me'
+			},			{
+				url: contact_path,
+				title: 'Contact'
+			},			{
+				url: blogs_path,
+				title: 'Blog'
+			},			{
+				url: portfolios_path,
+				title: 'Portfolio'
+			},
+		]
+	end
+
+	def nav_helper style, tag_type 
+		nav_links = ''
+
+		nav_items.each do |item|
+			nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a>
+			</#{tag_type}>"
+		end
+			nav_links.html_safe
+	end
+
+	def active? path
+		"active" if current_page? path
+	end
+
+	# def nav_helper style, tag_type
+	# nav_links = <<-NAV
+	# <#{tag_type}><a href="#{root_path}" class="#{style} #{active? root_path}">Home</a></#{tag_type}>
+	# <#{tag_type}><a href="#{about_path}" class="#{style} #{active? about_path}">About Me</a></#{tag_type}>
+	# <#{tag_type}><a href="#{contact_path}" class="#{style} #{active? contact_path}">Contact</a> </#{tag_type}>
+	# <#{tag_type}><a href="#{blogs_path}" class="#{style} #{active? blogs_path}">Blog</a> </#{tag_type}>
+	# <#{tag_type}><a href="#{portfolios_path}" class="#{style} #{active? portfolios_path}">Portfolio</a> </#{tag_type}>	
+	# NAV
+	# 	nav_links.html_safe
+	# end
+
 end
 
