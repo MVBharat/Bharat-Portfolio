5b8f5564e4c1b04b5fa61d2289fbde62f83a89b3
MVBharat <manebharat85@gmail.com>
2018-12-04 15:19:37 +0530

    Implemented persistant drag and drop portfolio items feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,6 +7,15 @@ class PortfoliosController < ApplicationController
 		@portfolio_items = Portfolio.by_position
 	end
 
+	def sort
+		params[:order].each do |key, value|
+			Portfolio.find(value[:id]).update(position: value[:position])
+		end
+
+		render nothing: true
+	end
+
+
 	def new
 		@portfolio_item = Portfolio.new
 		3.times { @portfolio_item.technologies.build }
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
-  resources :portfolios, except: [:show]
+  resources :portfolios, except: [:show] do
+    put :sort, on: :collection
+  end
   get 'portfolios/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   get 'about', to: 'pages#about'
